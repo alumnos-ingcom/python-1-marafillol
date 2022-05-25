36abae8ae77d3e7bc2ce75e63b933fc531c49f65
mara <marufillol@gmail.com>
2022-05-25 16:01:58 -0300

    Se ha creado el ejercicio2.py

---
+++ src/ejercicio2.py
@@ -0,0 +1,25 @@
+################
+# Mara Fillol - @marafillol
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def signo(numero):
+    """Esta función determina si el número ingresado es positivo, neutro o negativo.
+    """
+    calculo = numero + numero
+    if calculo > 0:
+        calculo = 'positivo'
+    elif calculo == 0:
+        calculo = 'neutro'
+    else:
+        calculo = 'negativo'
+    resultado = (f"{numero} es {calculo}")
+    return resultado
+def principal():
+    """
+    Esta función es la parte interactiva del programa.
+    """
+    numero = float(input("Ingrese un numero:  "))
+    print(signo(numero))
+if __name__ == "__main__":
+    principal()
