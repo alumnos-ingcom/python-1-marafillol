ce6fec6eabbdf1d2328b3c76fb2386e18e324569
mara <marufillol@gmail.com>
2022-05-31 12:19:16 -0300

    Se ha realizado el ejercicio6.py

---
+++ src/ejercicio6.py
@@ -0,0 +1,49 @@
+################
+# Mara Fillol - @marafillol
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+def ordenar_mayor_a_menor(uno, dos, tres):
+    """ Esta función ordena de mayor a menor los 3 valores ingresados.
+    """
+    if uno >= dos >= tres:
+        resultado = (uno, dos, tres)
+    elif uno >= tres >= dos:
+        resultado = (uno, tres, dos)
+    elif dos >= uno >= tres:
+        resultado = (dos, uno, tres)
+    elif dos >= tres >= uno:
+        resultado = (dos, tres, uno)
+    elif tres >= uno >= dos:
+        resultado = (tres, uno, dos)
+    elif tres >= dos >= uno:
+        resultado = (tres, dos, uno)
+    return resultado
+def ordenar_menor_a_mayor(uno, dos, tres):
+    """ Esta función ordena de menor a mayor los 3 valores ingresados.
+    """
+    if uno <= dos <= tres:
+        resultado = (uno, dos, tres)
+    elif uno <= tres <= dos:
+        resultado = (uno, tres, dos)
+        print(resultado)
+    elif dos <= uno <= tres:
+        resultado = (dos, uno, tres)
+    elif dos <= tres <= uno:
+        resultado = (dos, tres, uno)
+    elif tres <= uno <= dos:
+        resultado = (tres, uno, dos)
+    elif tres <= dos <= uno:
+        resultado = (tres, dos, uno)
+    return resultado
+def principal():
+    """
+    Esta función es la parte interactiva del programa.
+    """
+    uno = int(input("Ingrese un numero: "))
+    dos = int(input("Ingrese un numero: "))
+    tres = int(input("Ingrese un numero: "))
+    print(ordenar_mayor_a_menor(uno, dos, tres))
+    print(ordenar_menor_a_mayor(uno, dos, tres))
+if __name__ == "__main__":
+    principal()
+    
