f5851016f844be0861a4dea735fa2260eaf079a3
mara <marufillol@gmail.com>
2022-05-31 12:13:40 -0300

    Se ha realizado una modificacion en ejercicio1.py

--- src/ejercicio1.py
+++ src/ejercicio1.py
@@ -5,22 +5,22 @@
 def convertir_a_fahrenheit(centigrados):
     """Esta función convierte grados centigrados a grados fahrenheit.
     """
-    fahrenheit = (1.8 * centigrados) + 32
-    resultado = (f'{centigrados}° grados centigrados son {fahrenheit}° grados fahrenheit')
-    return resultado
+    resultado_fahrenheit = (1.8 * centigrados) + 32
+    return resultado_fahrenheit
 def convertir_a_centigrados(fahrenheit):
     """Esta función convierte grados fahrenheit a grados centigrados.
     """
-    centigrados = (fahrenheit - 32) * 5/9
-    resultado = (f'{fahrenheit}° grados fahrenheit son {centigrados}° grados centigrados')
-    return resultado
+    resultado_centigrados = (fahrenheit - 32) * 5/9
+    return resultado_centigrados
 def principal():
     """
     Esta función es la parte interactiva del programa.
     """
-    centigrados = float(input("Ingrese el grado que desea convertir: "))
-    fahrenheit = float(input("Ingrese el grado que desea convertir: "))
-    print(convertir_a_fahrenheit(centigrados))
-    print(convertir_a_centigrados(fahrenheit))
+    centigrados = float(input("Ingrese los grados centigrados: "))
+    fahrenheit = float(input("Ingrese los grados fahrenheit: "))
+    respuesta_de_f = convertir_a_fahrenheit(centigrados)
+    respuesta_de_c = convertir_a_centigrados(fahrenheit)
+    print(f"{centigrados}° centigrados son equivalentes a {respuesta_de_f}° fahrenheit.")
+    print(f"{fahrenheit}° fahrenheit son equivalentes a {respuesta_de_c}° centigrados.")
 if __name__ == "__main__":
     principal()
