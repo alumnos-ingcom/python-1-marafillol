abbb945cd9389766b9d6a4dbc978983ce821edb0
mara <marufillol@gmail.com>
2022-06-05 17:56:57 -0300

    Se ha realizado una modificacion en el ejercicio5.py

--- src/ejercicio5.py
+++ src/ejercicio5.py
@@ -2,23 +2,25 @@
 # Mara Fillol - @marafillol
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-def division_lenta(dividiendo, divisor):
-    """Esta función obtiene el cociente y resto de dos numeros enteros, mediante restas sucesivas.
+"""EJERCICIO 5
+"""
+def division_lenta(dividendo, divisor):
+    """Esta función obtiene el cociente y resto de dos números enteros, mediante restas sucesivas.
     """
     cociente = 0
-    while dividiendo >= divisor:
-        dividiendo = dividiendo - divisor
+    while dividendo >= divisor:
+        dividendo = dividendo - divisor
         cociente = cociente + 1
-    resto = dividiendo
-    resultado = cociente, resto
+    resto = dividendo
+    resultado = (cociente, resto)
     return resultado
 def principal():
     """
     Esta función es la parte interactiva del programa.
     """
-    dividiendo = int(input("Ingrese un numero: "))
+    dividendo = int(input("Ingrese un numero: "))
     divisor = int(input("Ingrese un numero: "))
-    respuesta = division_lenta(dividiendo, divisor)
+    respuesta = division_lenta(dividendo, divisor)
     print(f"El cociente es {respuesta[0]} y el resto es {respuesta[1]}.")
 if __name__ == "__main__":
     principal()
