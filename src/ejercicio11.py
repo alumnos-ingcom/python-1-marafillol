4714c34e48b89c4655ccd74265be106879241e46
mara <marufillol@gmail.com>
2022-06-06 11:27:30 -0300

    Se ha realizado una modificacion en ejercicio11.py

--- src/ejercicio11.py
+++ src/ejercicio11.py
@@ -5,12 +5,13 @@
 """EJERCICIO 11
 """
 def es_multiplo(numero, multiplo):
-    """Esta función que indica si un número entero es multiplo de otro, utilizando sumas y restas.
+    """Esta función indica si un número entero es multiplo de otro, utilizando sumas y restas.
     """
+    suma = 0
     if numero > 0 and multiplo > 0:
-        while numero < multiplo:
-            numero = numero + numero
-        resultado = numero == multiplo
+        while suma < multiplo:
+            suma = suma + numero
+        resultado = suma == multiplo
     else:
         resultado = "No ha ingresado un número entero positivo"
     return resultado
@@ -18,8 +19,8 @@ def principal():
     """
     Esta función es la parte interactiva del programa.
     """
-    numero = int(input('Ingrese un numero: '))
-    multiplo = int(input('Ingrese un multiplo: '))
+    numero = int(input('Ingrese un número: '))
+    multiplo = int(input('Ingrese un número: '))
     respuesta = es_multiplo(numero, multiplo)
     print(respuesta)
 if __name__ == "__main__":
